165ea719f7ab4bc1da3530c30108555096ec6a9a
Grace Liu <gracela@umich.edu>
2024-12-30 19:23:09 -0500

    finish static portions

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../src/components/header"
+import Main from "../src/components/main"
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  )
+}
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="src/assets/chef.png" alt="Chef Hat" />
+            <span>Chef Claude.</span>
+        </header>
+    )
+}
