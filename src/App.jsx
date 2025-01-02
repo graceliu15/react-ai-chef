c2cae99607cdec91efeaa886c7898799dd5ae494
Grace Liu <gracela@umich.edu>
2025-01-02 12:56:34 -0500

    render ingredient list and get recipe block

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,40 @@ export default function App() {
     </>
   )
 }
+
+
+
+
+
+
+// import React from "react"
+
+// export default function App() {
+//     /**
+//      * Challenge: 
+//      * Create state to track our count value (initial value is 0)
+//      * Don't forget to replace the hard-coded "0" with your new state
+//      */
+//     let [numClick, setNumClick] = React.useState("0")
+    
+//     function handleMinusClick() {
+//         setNumClick(prevNumClick => prevNumClick - 1)
+//     }
+    
+//     function handlePlusClick() {
+//         setNumClick(function(prevNumClick) {
+//           return prevNumClick + 1
+//         })
+//     }
+    
+//     return (
+//         <main className="container">
+//             <h1>How many times will Bob say "state" in this section?</h1>
+//             <div className="counter">
+//                 <button onClick={handleMinusClick} className="minus" aria-label="Decrease count">–</button>
+//                 <h2 className="count">{numClick}</h2>
+//                 <button onClick={handlePlusClick} className="plus" aria-label="Increase count">+</button>
+//             </div>
+//         </main>
+//     )
+// }
